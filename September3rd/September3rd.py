92949f5565c04c260fd12b36f3ce1a642a15c893
Eric Charnesky <eric.charnesky@careevolution.com>
2020-09-03 16:35:23 -0400

    examples with input for F to C

--- September3rd/September3rd.py
+++ September3rd/September3rd.py
@@ -1,1 +1,9 @@
 print('Hello world!')
+
+string_input = input("Please enter a temperature in F")
+temp_in_f = int(string_input)
+temp_in_c = (temp_in_f - 32) * 5 / 9
+
+# using a variable we haven't declared and given a value will give an error
+#print(temp_IN_c)
+print(temp_in_c)
