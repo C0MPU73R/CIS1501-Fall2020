fb7772d3ce6b044e815f24007ed6a7a77f458c3a
Eric Charnesky <eric.charnesky@careevolution.com>
2020-10-08 16:34:43 -0400

    moar methods

--- Chapter6-Functions/main.py
+++ Chapter6-Functions/main.py
@@ -1,3 +1,4 @@
+import random
 
 
 def print_hello():
@@ -23,8 +24,9 @@ print("Hi computer, I'm Eric")
 print(f"pi rounded to 3 digits is: {round(3.14159, 2)}")
 
 print_hello()
-
-count_down(10)
+my_number = 10
+count_down(my_number)
+print(my_number)
 
 # do not assign a function to a variable if it doesn't return a value
 result = count_down(20)
@@ -42,4 +44,23 @@ print(f'area of a 4x5 rectangle is {area_of_a_rectangle(4,5)}')
 print(f'area of a 4x5 rectangle is {area_of_a_rectangle("4",5)}')
 
 
-print("TODO for Thursday - start with Functions as Objections 6.8")
+print("TODO for Thursday - start with Functions as Objects 6.8")
+
+
+def normal_greeting():
+    print("Hello")
+
+
+def borg_greeting():
+    print("Prepare to be assimilated")
+
+
+def pretend_to_talk_to_people(greeting):
+    greeting()
+    print("What is your name?")
+
+# passing a function as an argument
+if random.randint(0,1) == 0:
+    pretend_to_talk_to_people(normal_greeting)
+else:
+    pretend_to_talk_to_people(borg_greeting)
